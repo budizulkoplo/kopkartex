fdd2b90e03adb3db947c683bf622de6925cb7f83
Dzulfikar Budi Andreanto <139447405+budizulkoplo@users.noreply.github.com>
2025-05-10 20:25:59 +0700

    update user acount picture

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -133,7 +133,7 @@
             </li>
             <li class="nav-item"> <a class="nav-link" href="#" data-lte-toggle="fullscreen"> <i data-lte-icon="maximize" class="bi bi-arrows-fullscreen"></i> <i data-lte-icon="minimize" class="bi bi-fullscreen-exit" style="display: none;"></i> </a> </li> <!--end::Fullscreen Toggle--> <!--begin::User Menu Dropdown-->
             <li class="nav-item dropdown user-menu"> 
-                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="../../../dist/assets/img/avatar5.png" class="user-image rounded-circle shadow" alt="User Image"></a>
+                <a href="#" class="nav-link dropdown-toggle" data-bs-toggle="dropdown"> <img src="{{ asset('user.png') }}" class="user-image rounded-circle shadow" alt="User Image"></a>
                 <ul class="dropdown-menu dropdown-menu-lg dropdown-menu-end"> <!--begin::User Image-->
                     <li class="user-header text-bg-primary" style="min-height: 0;"> 
                         <p class="profiles"> {{ Auth::user()->name }} <small>{{ Auth::user()->email }} - {{ strtoupper(auth()->user()->getRoleNames()[0]) }}</small></p>
