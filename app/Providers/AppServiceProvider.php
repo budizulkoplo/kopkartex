25d3d5192fbcd4a36b0ec79f642c990a89a6fc27
Dzulfikar Budi Andreanto <139447405+budizulkoplo@users.noreply.github.com>
2025-06-11 12:20:48 +0700

    fix https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -17,8 +16,6 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot()
     {
-    if (request()->header('x-forwarded-proto') == 'https') {
-        URL::forceScheme('https');
+        //
     }
-}
 }
