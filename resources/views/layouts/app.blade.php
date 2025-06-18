e62b4a132d8257041f28e4e2ed692af8a2a6a8e2
Agusta Bayu <inukage.inukage@gmail.com>
2025-06-18 21:20:38 +0700

    fix layout theme dark mode, Light and auto

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -97,6 +97,8 @@
             <!--end::Copyright-->
         </footer> <!--end::Footer-->
     </div> <!--end::App Wrapper--> <!--begin::Script--> <!--begin::Third Party Plugin(OverlayScrollbars)-->
+    
+</body><!--end::Body-->
     <script src="https://cdn.jsdelivr.net/npm/overlayscrollbars@2.3.0/browser/overlayscrollbars.browser.es6.min.js" integrity="sha256-H2VM7BKda+v2Z4+DRy69uknwxjyDRhszjXFhsL4gD3w=" crossorigin="anonymous"></script> <!--end::Third Party Plugin(OverlayScrollbars)--><!--begin::Required Plugin(popperjs for Bootstrap 5)-->
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha256-whL0tQWoY1Ku1iskqPFvmZ+CHsvmRWx/PIoEvIeWh4I=" crossorigin="anonymous"></script> <!--end::Required Plugin(popperjs for Bootstrap 5)--><!--begin::Required Plugin(Bootstrap 5)-->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha256-YMa+wAM6QkVyz999odX7lPRxkoYAan8suedu4k2Zur8=" crossorigin="anonymous"></script> <!--end::Required Plugin(Bootstrap 5)--><!--begin::Required Plugin(AdminLTE)-->
@@ -152,88 +154,87 @@
                 });
             }
         });
-        // // Color Mode Toggler
-        // (() => {
-        // "use strict";
+        // Color Mode Toggler
+        (() => {
+        "use strict";
 
-        // const storedTheme = localStorage.getItem("theme");
+        const storedTheme = localStorage.getItem("theme");
 
-        // const getPreferredTheme = () => {
-        //     if (storedTheme) {
-        //     return storedTheme;
-        //     }
+        const getPreferredTheme = () => {
+            if (storedTheme) {
+            return storedTheme;
+            }
 
-        //     return window.matchMedia("(prefers-color-scheme: dark)").matches
-        //     ? "dark"
-        //     : "light";
-        // };
+            return window.matchMedia("(prefers-color-scheme: dark)").matches
+            ? "dark"
+            : "light";
+        };
 
-        // const setTheme = function (theme) {
-        //     if (
-        //     theme === "auto" &&
-        //     window.matchMedia("(prefers-color-scheme: dark)").matches
-        //     ) {
-        //     document.documentElement.setAttribute("data-bs-theme", "dark");
-        //     } else {
-        //     document.documentElement.setAttribute("data-bs-theme", theme);
-        //     }
-        // };
+        const setTheme = function (theme) {
+            if (
+            theme === "auto" &&
+            window.matchMedia("(prefers-color-scheme: dark)").matches
+            ) {
+            document.documentElement.setAttribute("data-bs-theme", "dark");
+            } else {
+            document.documentElement.setAttribute("data-bs-theme", theme);
+            }
+        };
 
-        // setTheme(getPreferredTheme());
+        setTheme(getPreferredTheme());
 
-        // const showActiveTheme = (theme, focus = false) => {
-        //     const themeSwitcher = document.querySelector("#bd-theme");
+        const showActiveTheme = (theme, focus = false) => {
+            const themeSwitcher = document.querySelector("#bd-theme");
+            
+            if (!themeSwitcher) {
+            return;
+            }
 
-        //     if (!themeSwitcher) {
-        //     return;
-        //     }
+            const themeSwitcherText = document.querySelector("#bd-theme-text");
+            const activeThemeIcon = document.querySelector(".theme-icon-active i");
+            const btnToActive = document.querySelector(
+            `[data-bs-theme-value="${theme}"]`
+            );
+            if (!btnToActive) return;
+            const svgOfActiveBtn = btnToActive.querySelector("i").getAttribute("class");
 
-        //     const themeSwitcherText = document.querySelector("#bd-theme-text");
-        //     const activeThemeIcon = document.querySelector(".theme-icon-active i");
-        //     const btnToActive = document.querySelector(
-        //     `[data-bs-theme-value="${theme}"]`
-        //     );
-        //     const svgOfActiveBtn = btnToActive.querySelector("i").getAttribute("class");
+            for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
+            element.classList.remove("active");
+            element.setAttribute("aria-pressed", "false");
+            }
 
-        //     for (const element of document.querySelectorAll("[data-bs-theme-value]")) {
-        //     element.classList.remove("active");
-        //     element.setAttribute("aria-pressed", "false");
-        //     }
+            btnToActive.classList.add("active");
+            btnToActive.setAttribute("aria-pressed", "true");
+            activeThemeIcon.setAttribute("class", svgOfActiveBtn);
+            const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
+            themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
 
-        //     btnToActive.classList.add("active");
-        //     btnToActive.setAttribute("aria-pressed", "true");
-        //     activeThemeIcon.setAttribute("class", svgOfActiveBtn);
-        //     const themeSwitcherLabel = `${themeSwitcherText.textContent} (${btnToActive.dataset.bsThemeValue})`;
-        //     themeSwitcher.setAttribute("aria-label", themeSwitcherLabel);
+            if (focus) {
+            themeSwitcher.focus();
+            }
+        };
 
-        //     if (focus) {
-        //     themeSwitcher.focus();
-        //     }
-        // };
+        window
+            .matchMedia("(prefers-color-scheme: dark)")
+            .addEventListener("change", () => {
+            if (storedTheme !== "light" || storedTheme !== "dark") {
+                setTheme(getPreferredTheme());
+            }
+            });
 
-        // window
-        //     .matchMedia("(prefers-color-scheme: dark)")
-        //     .addEventListener("change", () => {
-        //     if (storedTheme !== "light" || storedTheme !== "dark") {
-        //         setTheme(getPreferredTheme());
-        //     }
-        //     });
+        window.addEventListener("DOMContentLoaded", () => {
+            showActiveTheme(getPreferredTheme());
 
-        // window.addEventListener("DOMContentLoaded", () => {
-        //     showActiveTheme(getPreferredTheme());
-
-        //     for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
-        //     toggle.addEventListener("click", () => {
-        //         const theme = toggle.getAttribute("data-bs-theme-value");
-        //         localStorage.setItem("theme", theme);
-        //         setTheme(theme);
-        //         showActiveTheme(theme, true);
-        //     });
-        //     }
-        // });
-        // })();
+            for (const toggle of document.querySelectorAll("[data-bs-theme-value]")) {
+            toggle.addEventListener("click", () => {
+                const theme = toggle.getAttribute("data-bs-theme-value");
+                localStorage.setItem("theme", theme);
+                setTheme(theme);
+                showActiveTheme(theme, true);
+            });
+            }
+        });
+        })();
         $( document ).ready(function() {$('#bdy').addClass('sidebar')});
     </script> <!--end::OverlayScrollbars Configure--> <!--end::Script-->
-</body><!--end::Body-->
-
 </html>
